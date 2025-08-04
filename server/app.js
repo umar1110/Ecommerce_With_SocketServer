b3cf3987e36998aabb299932ff605376f75012d6
umar1110 <umar10022004@gmail.com>
2025-08-04 22:53:16 +0500

    feat: redirect to frontend on "/*"

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const cors = require("cors");
 const User = require("./models/userModel");
+
 app.use(cookieParser());
 app.use(express.json({ extended: false }));
 
@@ -38,16 +39,14 @@ app.use("/api/v1/notification", NotificationRoutes);
 // Importing Middleware
 const errorMiddleware = require("./middlewares/error");
 app.use(errorMiddleware);
+// For Hosting
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
+// Serve static files from the React app's build directory
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// Handle all other routes by sending the main index.html file
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
 });
-// // Serve static files from the React app's build directory
-// app.use(express.static(path.join(__dirname, "../client/dist")));
-
-// // Handle all other routes by sending the main index.html file
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
-// });
 
 module.exports = app;
